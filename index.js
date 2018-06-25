223dec63d63276c8de080c4e28019cc9f4cd826e
Yi Shuen Lim <limyishuen@gmail.com>
2018-06-25 19:37:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  var n === name;
+  return "Happy holidays, ${name}!"
+}
+
+function happyHolidayTo(name, holiday) {
+  return "Happy ${holiday}, ${name}!"
+}
+
+function holidayCountdown(days, holiday) {
+  return "It's ${days} days until ${holiday}!"
+}
